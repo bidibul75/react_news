bce50b206fc9ba354d6470a36b5754050a7b4ded
bidibul75 <walter.bianchi@edu.esiee-it.fr>
2024-10-08 11:22:33 +0200

    Thèmes sur les modales.

--- src/components/ArticleModal.tsx
+++ src/components/ArticleModal.tsx
@@ -1,5 +1,5 @@
 // src/components/ArticleModal.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Article } from '../types/Article';
 
 interface ArticleModalProps {
@@ -9,11 +9,23 @@ interface ArticleModalProps {
 
 const ArticleModal: React.FC<ArticleModalProps> = ({ article, onClose }) => {
   if (!article) return null;
+  
+  let theme2=localStorage.getItem('theme');
+ 
+  var modal_theme, close_button;
+
+   if (theme2 == "light"){
+    modal_theme = "modal_back_light";
+    close_button = "close_button_light"
+   } else {
+    modal_theme = "modal_back_dark";
+    close_button = "close_button_dark"
+  }
 
   return (
     <div className="modal-overlay">
-      <div className="modal-content">
-        <button className="close-button" onClick={onClose}>
+      <div className={modal_theme}>
+        <button className={close_button} onClick={onClose}>
           &times;
         </button>
         <h2>{article.title}</h2>
